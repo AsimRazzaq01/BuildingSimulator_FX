2552dfbbaec8715f6730094d4efa57e0ce2d8b4c
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2024-04-15 20:53:33 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Run simulation with 2 elevators, 10 passengers, and 5 iterations
+     //   ElevatorSimulation simulation = new ElevatorSimulation(1, 100, 10);
+    //    simulation.runSimulation();
+
+    }
+}
+
+
+
---
+++ src/main/java/PatientPassanger.java
@@ -0,0 +1,17 @@
+public class PatientPassanger extends Passanger {
+
+    private int age;
+    private String PatientID;
+
+    public PatientPassanger(int startingFloor, int destinationFloor) {
+        super(startingFloor, destinationFloor);
+    }
+
+    @Override
+    boolean prefersExpressElevator() {
+        return false;
+    }
+    boolean isStaff() {
+        return false;
+    }
+}
